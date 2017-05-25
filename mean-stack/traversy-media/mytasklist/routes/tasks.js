8d9d77122d3a1346bf8fec678ac87143062cf716
David Piper <dbpiper@gmail.com>
2017-05-25 13:37:01 -0500

    Grab tasks from mongodb.

--- mean-stack/traversy-media/mytasklist/routes/tasks.js
+++ mean-stack/traversy-media/mytasklist/routes/tasks.js
@@ -1,8 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var mongojs = require('mongojs');
+var db = mongojs('mongodb://david:david@ds151941.mlab.com:51941/mytasklist',
+  ['tasks']);
 
+// Get All Tasks
 router.get('/tasks', function(req, res, next) {
-  res.send('TASK API');
+  db.tasks.find(function(err, tasks) {
+    if (err) {
+      res.send(err);
+    }
+    res.json(tasks);
+  });
 });
+
+// Get Single Task
+router.get('/task/:id', function(req, res, next) {
+  db.tasks.findOne({_id: mongojs.ObjectId(req.params.id)},
+    function(err, task) {
+      if (err) {
+        res.send(err);
+      }
+      res.json(task);
+    });
+});
+
 
 module.exports = router;
