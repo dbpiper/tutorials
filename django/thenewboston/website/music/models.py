8b0d7bfe82000b81ae1021109f5b2386823e1853
David Piper <dbpiper@gmail.com>
2017-05-31 10:30:21 -0500

    Go to the point in the thenewboston tutorial where a view is shown of all the albums.

--- django/thenewboston/website/music/models.py
+++ django/thenewboston/website/music/models.py
@@ -1,13 +1,15 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.db import models
 
 class Album(models.Model):
-    artist = models.CharField(max_len=250)
-    album_title = models.CharField(max_len=500)
-    genre = models.CharField(max_len=100)
-    album_logo = models.CharField(max_len=1000)
+    artist = models.CharField(max_length=250)
+    album_title = models.CharField(max_length=500)
+    genre = models.CharField(max_length=100)
+    album_logo = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.album_title + ' - ' + self.artist
 
 class Song(models.Model):
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    file_type = models.CharField(max_length=10)
+    song_title = models.CharField(max_length=250)
